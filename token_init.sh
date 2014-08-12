25a85a325030560cfcb1a0e0c15be35d5f480981
Petr Spacek <pspacek@redhat.com>
2014-08-12 09:21:33 +0200

    pridani PKCS11SPY do token_init.sh

--- token_init.sh
+++ token_init.sh
@@ -1,1 +1,2 @@
-pkcs11-tool --module /usr/lib64/softhsm/libsofthsm2.so  --so-pin 1234 --new-pin 1234 -l --init-pin --slot 0 --label IPA --init-token
+export PKCS11SPY=/usr/lib64/softhsm/libsofthsm2.so
+pkcs11-tool --module /usr/lib64/pkcs11-spy.so --so-pin 1234 --new-pin 1234 -l --init-pin --slot 0 --label IPA --init-token
